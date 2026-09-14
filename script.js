09e23927783e02673096fecf5fb47e7781f01688
Charles Price <charlieprice09@yahoo.com>
2026-09-14 18:21:49 -0500

    track clicks & js file

---
+++ script.js
@@ -0,0 +1,17 @@
+// get html elements
+const clickCount = document.getElementById('clickCount'); //click count
+const ben_img = document.getElementById("ben-img"); //image id
+
+// load storage
+let clicks = localStorage.getItem("clicks") || 0;
+
+clickCount.textContent = clicks; //update html display
+
+
+// event listeners
+
+ben_img.addEventListener("click", () => {
+    clicks++; //increment click count
+    clickCount.textContent = clicks; //update html display
+    localStorage.setItem("clicks", clicks); //save to local storage
+});
