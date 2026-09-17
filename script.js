f6dbc8b2b84b3648a824264dc5cb37f829caf5d4
charlespirce <charles.price1@wa-students.org>
2026-09-17 07:54:31 -0500

    put back click noise

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // get html elements
 const clickCount = document.getElementById('click-count'); //click count
 const ben_img = document.getElementById("ben-img"); //image id
+const click_noise = new Audio("mixkti-mouse-click-close-1113.wav"); //click noise
 
 // load storage
 let clicks = localStorage.getItem("totalClicks") || 0;
@@ -11,7 +12,7 @@ clickCount.textContent = clicks; //update html display
 // event listeners
 
 ben_img.addEventListener("click", () => {
-
+    click_noise.play();
     clicks++; //increment click count
     clickCount.textContent = clicks; //update html display
     localStorage.setItem("totalClicks", clicks); //save to local storage
