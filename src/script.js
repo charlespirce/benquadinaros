47bb8eff3cc5abc0d3c5d8ef1b490fd0bd1351d8
Charles Price <charlieprice09@yahoo.com>
2026-09-20 13:41:34 -0500

    correct src

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // get html elements
 const clickCount = document.getElementById('click-count'); //click count
 const ben_img = document.getElementById("ben-img"); //image id
-const click_noise = new Audio("mixkit-mouse-click-close-1113.wav"); //click noise
+const click_noise = new Audio("assets/mixkit-mouse-click-close-1113.wav"); //click noise
 const reset_button = document.getElementById('reset-clicks');
 const disp_rate = document.getElementById("display-rate");
 //rat
