c82460cdb9083a2e64755e65b6d58517ab36f534
charlespirce <charles.price1@wa-students.org>
2026-09-25 09:32:59 -0500

    ?

--- src/test.js
+++ src/test.js
@@ -66,7 +66,7 @@ const hoverDuration = 400;
 let hoverTimer;
 
 function changeSpeed() {
-    var rand_theta = (Math.random()*2*Math.PI) 
+    let rand_theta = (Math.random()*2*Math.PI) 
   xSpeed = 7*Math.cos(rand_theta);
   ySpeed = 7*Math.sin(rand_theta);
 }
